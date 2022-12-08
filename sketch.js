dfee1e5bb25c8e5019c45e776c7401fd4ba7f355
Jacson Curtis <justjcurtis@gmail.com>
2022-12-08 22:12:58 +0000

    perf: optimise

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ let h
 let qt
 let points
 let pointCount = 200
+let lineDist
 
 const xRandomPoints = (x, w, h) => {
   let p = []
@@ -15,6 +16,7 @@ const xRandomPoints = (x, w, h) => {
 function setup() {
   w = window.innerWidth
   h = window.innerHeight
+  lineDist = Math.max(w, h) / 10
   points = xRandomPoints(pointCount, w, h)
   qt = new QuadTree(w, h)
   for (let i = 0; i < points.length; i++) {
@@ -28,11 +30,15 @@ function draw() {
   background(0)
   for (let i = 0; i < points.length; i++) {
     const point = points[i]
-    point.render(qt)
+    point.render(qt, lineDist)
     point.update(qt)
     qt.remove(point)
     qt.insert(point)
-
   }
   qt.join()
+
+  textSize(10);
+  fill(255);
+  stroke(0);
+  text((frameRate()).toFixed(2), 5, 10);
 }
